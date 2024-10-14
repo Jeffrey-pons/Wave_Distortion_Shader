e624a96fe1e1fa94857ee80a2406986079d893e6
JeffreyPons <ponsjeffrey@gmail.com>
2024-10-14 15:02:53 +0200

    adding vertex shader, texture img

---
+++ src/components/Shader.jsx
@@ -0,0 +1,24 @@
+export const fragment = `
+
+    void main() {
+
+        gl_FragColor = vec4(1., 0., 0., 1.);
+
+    }
+
+`
+
+export const vertex = `
+    uniform float uTime;
+    uniform float uAmplitude;
+    uniform float uWaveLength;
+    void main() {
+        vec3 newPosition = position;
+
+        float wave = uAmplitude * sin(position.x * uWaveLength + uTime);
+        newPosition.z = position.z + wave;
+
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);
+    }
+
+`
